516704f5f0ddcaff34e36b6af55f9b21e4c1ef29
FURAHA NIYONGIRA Celia <furahacelia33@gmail.com>
2026-01-22 10:16:14 +0200

    Updated App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 import WelcomeScreen from './screens/WelcomeScreen';
 import DiscoveryScreen from './screens/DiscoveryScreen';
 import UpdateScreen from '../screens/UpdateScreen';
+import ExploreScreen from '../screens/ExploreScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,7 @@ export default function App() {
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Discovery" component={DiscoveryScreen} />
         <Stack.Screen name='Update' component={UpdateScreen}/>
+        <Stack.Screen name='Explore' component={ExploreScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
